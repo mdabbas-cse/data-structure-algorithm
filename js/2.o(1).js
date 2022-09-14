0de4ace9865a3ef91268d2c4b371b0cb5450bb32
mdabbas-cse <gmabbas44@gmail.com>
2022-09-14 23:47:08 +0600

    big o(1) check

---
+++ js/2.o(1).js
@@ -0,0 +1,10 @@
+/**
+ * Big Operation notation O(1)
+ */
+const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const findNumber = (dataArr) => {
+  console.log(dataArr[0]) //O(1)
+  console.log(dataArr[1]) // O(1)
+}
+
+findNumber(number) // O(2) Constant Time
