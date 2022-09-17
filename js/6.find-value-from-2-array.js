2b9986412e93b8629d54a8f5df23ae80a14a92c7
mdabbas-cse <gmabbas44@gmail.com>
2022-09-17 23:44:21 +0600

    upldate find value from array

--- js/6.find-value-from-2-array.js
+++ js/6.find-value-from-2-array.js
@@ -33,6 +33,15 @@ function containsCommonItem2(arr1, arr2) {
   }
   return false
 }
+
+function betterSolution(arr, sum) {
+  const mySet = new Set()
+  for (let i = 0; i < arr.length; i++) {
+    if (mySet.has(arr[i])) return true
+    mySet.add(sum - arr[i])
+  }
+  return false
+}
 //O(a + b) Time Complexity
 //O(a) Space Complexity
 // 👉 Second Solution
@@ -44,3 +53,4 @@ const array2 = ['z', 'y', 'd'];
 console.log(containsCommonItem(array1, array2))
 console.log(containsCommonItem2(array1, array2))
 console.log(containsCommonItem3(array1, array2))
+console.log(betterSolution([1, 2, 3, 4, 5], 5))
