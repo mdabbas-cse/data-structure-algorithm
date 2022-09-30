23c37547c7569c63057e70b610270c2de888d07b
mdabbas-cse <gmabbas44@gmail.com>
2022-09-30 09:56:33 +0600

    linked list init

---
+++ 6.Link-list/js/1.Link-list.js
@@ -0,0 +1,29 @@
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null
+    }
+    this.tail = this.head
+    this.length = 1
+  }
+
+  append(value) {
+    const newNode = {
+      value: value,
+      next: null
+    }
+    this.tail.next = newNode
+    this.tail = newNode
+    this.length++
+    return this
+  }
+}
+
+const linkedlist = new LinkedList(15)
+linkedlist
+  .append(5)
+  .append(52)
+  .append(12)
+  .append(20)
+console.log(linkedlist)
