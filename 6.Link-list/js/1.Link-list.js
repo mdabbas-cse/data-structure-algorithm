bc15494b642a4eefbe9034d3a0015ed5926eeeed
mdabbas-cse <gmabbas44@gmail.com>
2022-09-30 16:18:52 +0600

    add insert method for insert value in Linked list

--- 6.Link-list/js/1.Link-list.js
+++ 6.Link-list/js/1.Link-list.js
@@ -31,17 +31,49 @@ class LinkedList {
     return this
   }
 
-  printList() {
-    const arrayList = []
+  insert(index, value) {
+    // check index
+    if (index === 0)
+      return this.prepend(value)
+
+    if (index >= this.length)
+      return this.append(value)
+
+    const newNode = new Node(value)
+    const leader = this.#traverseToIndex(index - 1)
+    const holdingPointer = leader.next
+    leader.next = newNode
+    newNode.next = holdingPointer
+    this.length++
+    // console.log('leader', leader.next)
+    return this
+  }
+
+  #traverseToIndex(previusIndex) {
     let currentNode = this.head
-    while (currentNode !== null) {
-      arrayList.push(currentNode.value)
+    let counter = 0
+    while (counter !== previusIndex) {
       currentNode = currentNode.next
+      counter++
+    }
+    return currentNode
+  }
+
+  printList() {
+    const arrayList = {}
+    let currentNode = this.head
+    let propertyId = 0
+    while (currentNode !== null) {
+      arrayList[propertyId] = currentNode.value
+      currentNode = currentNode.next
+      propertyId++
     }
     console.log(arrayList)
   }
 }
 
+
+console.clear()
 const linkedlist = new LinkedList(15)
 linkedlist
   .append(5)
@@ -49,5 +81,8 @@ linkedlist
   .append(12)
   .append(20)
   .prepend(1)
+  .insert(10, 99)
+  .insert(0, 88)
+  .insert(3, 77)
   .printList()
 // console.log(linkedlist)
