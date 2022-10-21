b85d5b1552e81b58de4ee40b08d9db15902f54db
mdabbas-cse <gmabbas44@gmail.com>
2022-10-21 16:59:20 +0600

    Binary Tree implement with pythond

--- 9.Binary-tree/python/1.Binary-tree.py
+++ 9.Binary-tree/python/1.Binary-tree.py
@@ -1,27 +1,60 @@
-from '0.Node' import Node
+import json
+
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
 
 class BinaryTree:
-  def __init__(self):
-    self.root = None
-  
-  def insert(self, value):
-    newNode = Node(value)
-    if self.root is None:
-      self.root = newNode
-      return True
-    currentNode = self.root
-    while(True):
-      if value < currentNode.value:
-        if currentNode.left is None:
-          currentNode.left = newNode
-          return True
-        currentNode = currentNode.left
-      else:
-        if currentNode.right is None:
-          currentNode.right = newNode
-          return True
-        currentNode = currentNode.right
+    def __init__(self):
+        self.root = None
+        self.displayData = None
 
+    def insert(self, value):
+        newNode = Node(value)
+        if self.root is None:
+            self.root = newNode
+            return True
+        currentNode = self.root
+        while currentNode:
+            if value < currentNode.value:
+                if currentNode.left is None:
+                    currentNode.left = newNode
+                    return self
+                currentNode = currentNode.left
+            else:
+                if currentNode.right is None:
+                    currentNode.right = newNode
+                    return self
+                currentNode = currentNode.right
+        self.displayData = self.root
+        return self
+
+    def lookup(self, value):
+        if self.root is None:
+            return False
+        currentNode = self.root
+        while currentNode:
+            if value < currentNode.value:
+                currentNode = currentNode.left
+            elif value > currentNode.value:
+                currentNode = currentNode.right
+            elif currentNode.value == value:
+                self.displayData = currentNode
+                return self
+
+    def display(self):
+        if self.displayData is None:
+            print("No data to display")
+            return False
+        else:
+            root = self.displayData
+            convert = json.dumps(self.displayData.__dict__)
+            print(convert)
+            return True
 
 
 tree = BinaryTree()
@@ -32,5 +65,6 @@ tree.insert(20)
 tree.insert(170)
 tree.insert(15)
 tree.insert(1)
+tree.display()
 
-print(tree.root.left.left.value)
+tree.lookup(1).display()
