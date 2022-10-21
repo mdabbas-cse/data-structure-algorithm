15192b5cd2663875c6bc26bae3a4ac7de6564209
mdabbas-cse <gmabbas44@gmail.com>
2022-10-21 23:20:35 +0600

    add min and max method for search min/max value from tree

--- 9.Binary-tree/js/1.tree.js
+++ 9.Binary-tree/js/1.tree.js
@@ -54,11 +54,55 @@ class BinaryTree {
     }
   }
 
+
+  // DFS traversal
   preOrder(root) {
     if (!root) return
     console.log(root.value)
     this.preOrder(root.left)
     this.preOrder(root.right)
+  }
+
+  inOrder(root) {
+    if (!root) return
+    this.inOrder(root.left)
+    console.log(root.value)
+    this.inOrder(root.right)
+  }
+
+  postOrder(root) {
+    if (!root) return
+    this.postOrder(root.left)
+    this.postOrder(root.right)
+    console.log(root.value)
+  }
+
+  // BFS traversal
+  lavalOrder() {
+    const queue = []
+    queue.push(this.root)
+    while (queue.length) {
+      const node = queue.shift()
+      console.log(node.value)
+      if (node.left) queue.push(node.left)
+      if (node.right) queue.push(node.right)
+    }
+  }
+
+  // min value 
+  min(root) {
+    if (!root.left)
+      return root.value
+    else
+      return this.min(root.left)
+  }
+
+  // max depth
+  max(root) {
+    if (!root.right)
+      return root.value
+    else
+      return this.max(root.right)
   }
 
   remove(value) {
@@ -70,18 +114,22 @@ class BinaryTree {
     console.log(data)
   }
 }
-// https://www.youtube.com/watch?v=80GhW9X1MGI&ab_channel=Codevolution
+// https://www.youtube.com/watch?v=mrzE5SqzoQY&list=PLC3y8-rFHvwg6nsAOfC5Is18KB2DrVOJy&index=38&ab_channel=Codevolution
 // https://www.youtube.com/watch?v=gcULXE7ViZw&ab_channel=mycodeschool
 console.clear()
 const binaryTree = new BinaryTree()
 binaryTree.insert(9)
-binaryTree.insert(4)
-binaryTree.insert(20)
-binaryTree.insert(1)
-binaryTree.insert(6)
+binaryTree.insert(5)
 binaryTree.insert(15)
-binaryTree.insert(170).display()
+binaryTree.insert(3)
+binaryTree.insert(7)
+display(binaryTree.root)
 // console.log(JSON.stringify(binaryTree.root))
 // display(binaryTree.root)
 // display(binaryTree.lookup(binaryTree.root, 29))
-binaryTree.preOrder(binaryTree.root)
+// binaryTree.preOrder(binaryTree.root)
+// binaryTree.inOrder(binaryTree.root)
+// binaryTree.postOrder(binaryTree.root)
+// binaryTree.lavalOrder()
+display(binaryTree.min(binaryTree.root))
+display(binaryTree.max(binaryTree.root))
