e5193262a9ffd2607ff89e78a83fee9c1cbbc508
Tanvir Chowdhury <76895700+mdabbas-cse@users.noreply.github.com>
2023-06-10 22:52:15 +0600

    move all in data structure folder

--- 3.Reference-type/js/1.reference-type.js
+++
@@ -1,9 +0,0 @@
-// reference type
-const Object1 = { val: 10 }
-const Object2 = Object1
-const Object3 = { val: 10 }
-
-Object1.val = 20 // we reassing val property value is 20, but not change this reference so Object1 & Object2 true
-
-console.log(Object1 === Object2) // output: true; because stored object reference
-console.log(Object1 === Object3) // output: false; because 2 object reference are not same
